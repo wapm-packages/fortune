05938e3ba2e135d9cb7985dcc15d22cefc82456a
dennemann <cb.nitusen@gmail.com>
2019-03-24 16:29:38 +0100

    Fix for file not found when using symlinks.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::fs;
+use std::env;
 use std::io;
-use std::path::PathBuf;
+use std::path::Path;
 use rand::Rng;
 
 fn main() -> io::Result<()> {
-    let path = PathBuf::from("fortunes");
-    let f = fs::read_to_string(path)?;
+
+    // Lazy fix for bug of not finding fortunes.txt when creating a symlink on linux.
+    // hopepfully this should work on Windows too. Have not tried it yet.
+    let tmp = std::env::current_exe()?;
+    let tmp2 = tmp.parent().unwrap();
+    let dir = Path::new(tmp2);
+    let file = Path::new("fortunes.txt");
+    let file_dir = dir.join(file);
+
+    // Start reading, collecting and printing.
+    let f = fs::read_to_string(file_dir)?;
     let quotes: Vec<&str> = f.split("\n%\n").collect();
 
     let mut random = rand::thread_rng();
