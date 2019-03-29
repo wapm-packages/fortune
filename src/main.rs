933884dd64970bdb525ac314becfd3f4367dc82f
dennemann <cb.nitusen@gmail.com>
2019-03-29 14:34:56 +0100

    Some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,51 @@
 use std::fs;
-use std::env;
 use std::io;
 use std::path::Path;
 use rand::Rng;
 
 fn main() -> io::Result<()> {
 
-    // Lazy fix for bug of not finding fortunes.txt when creating a symlink on linux.
-    // hopepfully this should work on Windows too. Have not tried it yet.
-    let tmp = std::env::current_exe()?;
-    let tmp2 = tmp.parent().unwrap();
-    let dir = Path::new(tmp2);
-    let file = Path::new("fortunes.txt");
-    let file_dir = dir.join(file);
+    let file = read_file().unwrap();
 
-    // Start reading, collecting and printing.
-    let f = fs::read_to_string(file_dir)?;
-    let quotes: Vec<&str> = f.split("\n%\n").collect();
+    let quotes: Vec<&str> = file.split("\n%\n").collect();
 
-    let mut random = rand::thread_rng();
-    let r_num = random.gen_range(0, quotes.len() - 1);
+    let mut r_thread = rand::thread_rng();
+    let r_num = r_thread.gen_range(0, quotes.len() - 1);
 
     for q in quotes[r_num].chars() {
         print!("{}", q);
     }
 
     Ok(())
+}
+
+fn read_file() -> Result<String, &'static str> {
+
+    let quotebase = directory(std::path::Path::new("fortunes.txt")).unwrap();
+
+    let file = match fs::read_to_string(quotebase) {
+        Ok(f) => f,
+        Err(_) => return Err("Could not open file"),
+    };
+
+    Ok(file)
+
+}
+
+fn directory<F: AsRef<Path>>(file: F) -> Result<std::path::PathBuf, &'static str> {
+    
+    let exe_path = match std::env::current_exe() {
+        Ok(f) => f,
+        Err(_) => return Err("Can't find executable"),
+    };
+    
+    let exe_parent_path = match exe_path.parent() {
+        Some(f) => f,
+        None => return Err("Can't get executable's parent path."),
+    };
+
+    let path = exe_parent_path.join(file);
+
+    Ok(path)
+
 }
